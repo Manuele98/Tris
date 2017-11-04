1a1065a4102d5efb87303153b77f8440a253b516
andre <andre@192.168.1.102>
2017-11-04 19:30:00 +0100

    Aggiunta dei main e continuato Client

--- ProgettoTris/src/CONTROLLER/Client.java
+++ ProgettoTris/src/CONTROLLER/Client.java
@@ -3,20 +3,36 @@ package CONTROLLER;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
+import javax.swing.JOptionPane;
+
+import MODEL.Persona;
 import VIEW.Finestra1;
+import VIEW.Finestra2;
 
 public class Client implements ActionListener{
 	private Finestra1 f1;
+	private Finestra2 f2;
+	private Persona p;
 	
-	public Client (Finestra1 f1){
+	public Client (Finestra1 f1, Finestra2 f2){
 		this.f1=f1;
+		this.f2=f2;
+		f1.getBtn_Accedi().addActionListener(this);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent evt) {
-		/*if (evt.getSource()){
-			
-		}*/
+		if (evt.getSource()==f1.getBtn_Accedi()){
+			String nome=(String)f1.getTextField_NickName().getText();
+			if (nome.equals("")) {
+				JOptionPane.showMessageDialog(f1, "Inserisci un nickname", "ERRORE", 0);
+			}
+			else {
+				p=new Persona(nome);
+				f2.setVisible(true);
+				f1.setVisible(false);
+			}
+		}
 	}
 	
 }
--- ProgettoTris/src/MODEL/Persona.java
+++ ProgettoTris/src/MODEL/Persona.java
@@ -1,5 +1,27 @@
 package MODEL;
 
 public class Persona {
+	private String nickname;
+	private int vinte;
+	
+	public Persona (String nickname) {
+		this.nickname=nickname;
+	}
 
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public int getVinte() {
+		return vinte;
+	}
+
+	public void setVinte(int vinte) {
+		this.vinte = vinte;
+	}
+	
 }
