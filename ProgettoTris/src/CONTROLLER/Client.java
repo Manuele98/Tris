4eac2c5421eb165d4d982b5e5e36a922f3cc20c0
andrea <andre@192.168.1.102>
2017-11-05 11:44:55 +0100

    Implementazione MouseListener

--- ProgettoTris/src/CONTROLLER/Client.java
+++ ProgettoTris/src/CONTROLLER/Client.java
@@ -2,6 +2,8 @@ package CONTROLLER;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JOptionPane;
 
@@ -9,15 +11,26 @@ import MODEL.Persona;
 import VIEW.Finestra1;
 import VIEW.Finestra2;
 
-public class Client implements ActionListener{
+public class Client implements ActionListener, MouseListener{
 	private Finestra1 f1;
 	private Finestra2 f2;
 	private Persona p;
+	private boolean flag=false;
 	
 	public Client (Finestra1 f1, Finestra2 f2){
 		this.f1=f1;
 		this.f2=f2;
 		f1.getBtn_Accedi().addActionListener(this);
+		f2.getBtn_Invia().addActionListener(this);
+		f2.getPanel_1().addMouseListener(this);
+		f2.getPanel_2().addMouseListener(this);
+		f2.getPanel_3().addMouseListener(this);
+		f2.getPanel_4().addMouseListener(this);
+		f2.getPanel_5().addMouseListener(this);
+		f2.getPanel_6().addMouseListener(this);
+		f2.getPanel_7().addMouseListener(this);
+		f2.getPanel_8().addMouseListener(this);
+		f2.getPanel_9().addMouseListener(this);
 	}
 
 	@Override
@@ -33,6 +46,81 @@ public class Client implements ActionListener{
 				f1.setVisible(false);
 			}
 		}
+		if (evt.getSource()==f2.getBtn_Invia()) {
+			if (flag==true) {
+				// Inviare al server la posizione selezionata
+				
+				
+				flag=false;
+			}
+			else {
+				JOptionPane.showMessageDialog(f2, "Seleziona un pannello", "ERRORE", 0);
+			}
+		}
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent evt) {
+		if (evt.getSource()==f2.getPanel_1()) {
+			System.out.println("Pannello 1 cliccato");
+			flag=true;
+		}
+		if (evt.getSource()==f2.getPanel_2()) {
+			System.out.println("Pannello 2 cliccato");
+			flag=true;
+		}
+		if (evt.getSource()==f2.getPanel_3()) {
+			System.out.println("Pannello 3 cliccato");
+			flag=true;
+		}
+		if (evt.getSource()==f2.getPanel_4()) {
+			System.out.println("Pannello 4 cliccato");
+			flag=true;
+		}
+		if (evt.getSource()==f2.getPanel_5()) {
+			System.out.println("Pannello 5 cliccato");
+			flag=true;
+		}
+		if (evt.getSource()==f2.getPanel_6()) {
+			System.out.println("Pannello 6 cliccato");
+			flag=true;
+		}
+		if (evt.getSource()==f2.getPanel_7()) {
+			System.out.println("Pannello 7 cliccato");
+			flag=true;
+		}
+		if (evt.getSource()==f2.getPanel_8()) {
+			System.out.println("Pannello 8 cliccato");
+			flag=true;
+		}
+		if (evt.getSource()==f2.getPanel_9()) {
+			System.out.println("Pannello 9 cliccato");
+			flag=true;
+		}
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent evt) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent evt) {
+		// TODO Auto-generated method stub
+		
 	}
 	
 }
