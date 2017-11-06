12e3bab62e4698d734a478d2eed786c9b56bfb71
Alunno <Alunno@PC-11-INF4>
2017-11-06 12:29:08 +0100

    Correzione componenti grafici e aggiunta delle immagini ridimensionate

--- ProgettoTris/src/VIEW/Finestra2.java
+++ ProgettoTris/src/VIEW/Finestra2.java
@@ -11,31 +11,23 @@ import javax.swing.JLabel;
 import java.awt.Font;
 import javax.swing.SwingConstants;
 import javax.swing.JButton;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 
 public class Finestra2 extends JFrame {
 
 	private JPanel contentPane;
 	private JPanel panel_Nero_1_vert;
-	private JPanel panel_1;
 	private JPanel panel_Nero_2_vert;
-	private JPanel panel_3;
-	private JPanel panel_2;
 	private JPanel panel_Nero_1_orz;
-	private JPanel panel_4;
 	private JPanel panel_Nero_2_orz;
-	private JPanel panel_7;
-	private JPanel panel_5;
-	private JPanel panel_6;
-	private JPanel panel_8;
-	private JPanel panel_9;
 	private JLabel lbl_Simbolo_Gioco;
-	private JPanel panel_Simbolo_Giocatore;
 	private JLabel lbl_Avversario;
 	private JButton btn_Invia;
-	private JPanel panel_Simbolo_Avversario;
 	private JLabel lbl_Titolo;
 	private JButton btnEsci;
 	private JLabel lblLabelNomeAvversario;
+	private ImageIcon icon,icon2;
 	private JLabel lbl_1;
 	private JLabel lbl_2;
 	private JLabel lbl_3;
@@ -45,6 +37,8 @@ public class Finestra2 extends JFrame {
 	private JLabel lbl_7;
 	private JLabel lbl_8;
 	private JLabel lbl_9;
+	private JLabel label_Simbolo_Giocatore;
+	private JLabel label_Simbolo_Avversario;
 
 	/**
 	 * Launch the application.
@@ -77,112 +71,27 @@ public class Finestra2 extends JFrame {
 		panel_Nero_1_vert.setBackground(Color.BLACK);
 		panel_Nero_1_vert.setBounds(192, 159, 10, 433);
 		contentPane.add(panel_Nero_1_vert);
-		
-		panel_1 = new JPanel();
-		panel_1.setBounds(41, 159, 142, 123);
-		contentPane.add(panel_1);
-		panel_1.setLayout(null);
-		
-		lbl_1 = new JLabel("");
-		lbl_1.setBounds(0, 0, 142, 123);
-		panel_1.add(lbl_1);
-		
+				
 		panel_Nero_2_vert = new JPanel();
 		panel_Nero_2_vert.setBackground(Color.BLACK);
 		panel_Nero_2_vert.setBounds(364, 159, 10, 433);
 		contentPane.add(panel_Nero_2_vert);
-		
-		panel_3 = new JPanel();
-		panel_3.setBounds(384, 159, 142, 123);
-		contentPane.add(panel_3);
-		panel_3.setLayout(null);
-		
-		lbl_3 = new JLabel("");
-		lbl_3.setBounds(0, 0, 142, 123);
-		panel_3.add(lbl_3);
-		
-		panel_2 = new JPanel();
-		panel_2.setBounds(212, 159, 142, 123);
-		contentPane.add(panel_2);
-		panel_2.setLayout(null);
-		
-		lbl_2 = new JLabel("");
-		lbl_2.setBounds(0, 0, 142, 123);
-		panel_2.add(lbl_2);
 		
 		panel_Nero_1_orz = new JPanel();
 		panel_Nero_1_orz.setBackground(Color.BLACK);
 		panel_Nero_1_orz.setBounds(41, 293, 487, 10);
 		contentPane.add(panel_Nero_1_orz);
 		
-		panel_4 = new JPanel();
-		panel_4.setBounds(41, 314, 142, 123);
-		contentPane.add(panel_4);
-		panel_4.setLayout(null);
-		
-		lbl_4 = new JLabel("");
-		lbl_4.setBounds(0, 0, 142, 123);
-		panel_4.add(lbl_4);
-		
 		panel_Nero_2_orz = new JPanel();
 		panel_Nero_2_orz.setBackground(Color.BLACK);
 		panel_Nero_2_orz.setBounds(41, 448, 487, 10);
 		contentPane.add(panel_Nero_2_orz);
-		
-		panel_7 = new JPanel();
-		panel_7.setBounds(41, 469, 142, 123);
-		contentPane.add(panel_7);
-		panel_7.setLayout(null);
-		
-		lbl_7 = new JLabel("");
-		lbl_7.setBounds(0, 0, 142, 123);
-		panel_7.add(lbl_7);
-		
-		panel_5 = new JPanel();
-		panel_5.setBounds(212, 314, 142, 123);
-		contentPane.add(panel_5);
-		panel_5.setLayout(null);
-		
-		lbl_5 = new JLabel("");
-		lbl_5.setBounds(0, 0, 142, 123);
-		panel_5.add(lbl_5);
-		
-		panel_6 = new JPanel();
-		panel_6.setBounds(384, 314, 142, 123);
-		contentPane.add(panel_6);
-		panel_6.setLayout(null);
-		
-		lbl_6 = new JLabel("");
-		lbl_6.setBounds(0, 0, 142, 123);
-		panel_6.add(lbl_6);
-		
-		panel_8 = new JPanel();
-		panel_8.setBounds(212, 469, 142, 123);
-		contentPane.add(panel_8);
-		panel_8.setLayout(null);
-		
-		lbl_8 = new JLabel("");
-		lbl_8.setBounds(0, 0, 142, 123);
-		panel_8.add(lbl_8);
-		
-		panel_9 = new JPanel();
-		panel_9.setBounds(384, 469, 142, 123);
-		contentPane.add(panel_9);
-		panel_9.setLayout(null);
-		
-		lbl_9 = new JLabel("");
-		lbl_9.setBounds(0, 0, 142, 123);
-		panel_9.add(lbl_9);
 		
 		lbl_Simbolo_Gioco = new JLabel("Tu giochi con -->");
 		lbl_Simbolo_Gioco.setHorizontalAlignment(SwingConstants.CENTER);
 		lbl_Simbolo_Gioco.setFont(new Font("Lucida Calligraphy", Font.PLAIN, 20));
 		lbl_Simbolo_Gioco.setBounds(595, 178, 181, 35);
 		contentPane.add(lbl_Simbolo_Gioco);
-		
-		panel_Simbolo_Giocatore = new JPanel();
-		panel_Simbolo_Giocatore.setBounds(788, 159, 65, 65);
-		contentPane.add(panel_Simbolo_Giocatore);
 		
 		lbl_Avversario = new JLabel("Avversario -->");
 		lbl_Avversario.setFont(new Font("Lucida Calligraphy", Font.PLAIN, 20));
@@ -194,10 +103,6 @@ public class Finestra2 extends JFrame {
 		btn_Invia.setFont(new Font("Tahoma", Font.PLAIN, 16));
 		btn_Invia.setBounds(709, 448, 110, 35);
 		contentPane.add(btn_Invia);
-		
-		panel_Simbolo_Avversario = new JPanel();
-		panel_Simbolo_Avversario.setBounds(788, 293, 65, 65);
-		contentPane.add(panel_Simbolo_Avversario);
 		
 		lbl_Titolo = new JLabel("Tris & Vinci");
 		lbl_Titolo.setHorizontalAlignment(SwingConstants.CENTER);
@@ -215,14 +120,74 @@ public class Finestra2 extends JFrame {
 		lblLabelNomeAvversario.setBounds(595, 362, 181, 35);
 		contentPane.add(lblLabelNomeAvversario);
 		
+		lbl_1 = new JLabel("");
+		lbl_1.setBounds(40, 159, 142, 123);
+		icon = new ImageIcon("Cerchio.png");     // istruzioni necessarie per far comparire l'immagine sul tris
+		lbl_1.setIcon(icon);                     // istruzioni necessarie per far comparire l'immagine sul tris
+		contentPane.add(lbl_1);
+		
+		lbl_2 = new JLabel("");
+		lbl_2.setBounds(212, 159, 142, 123);
+		icon = new ImageIcon("X.jpg");           // istruzioni necessarie per far comparire l'immagine sul tris
+		lbl_2.setIcon(icon);                     // istruzioni necessarie per far comparire l'immagine sul tris
+		contentPane.add(lbl_2);  
+		
+		lbl_3 = new JLabel("");
+		lbl_3.setBounds(384, 159, 142, 123);
+		contentPane.add(lbl_3);
+		
+		lbl_4 = new JLabel("");
+		lbl_4.setBounds(41, 314, 142, 123);
+		contentPane.add(lbl_4);
+		
+		lbl_5 = new JLabel("");
+		lbl_5.setBounds(212, 314, 142, 123);
+		contentPane.add(lbl_5);
+		
+		lbl_6 = new JLabel("");
+		lbl_6.setBounds(386, 314, 142, 123);
+		contentPane.add(lbl_6);
+		
+		lbl_7 = new JLabel("");
+		lbl_7.setBounds(40, 469, 142, 123);
+		contentPane.add(lbl_7);
+		
+		lbl_8 = new JLabel("");
+		lbl_8.setBounds(212, 469, 142, 123);
+		contentPane.add(lbl_8);
+		
+		lbl_9 = new JLabel("");
+		lbl_9.setBounds(386, 469, 142, 123);
+		contentPane.add(lbl_9);
+		
+		label_Simbolo_Giocatore = new JLabel("");
+		label_Simbolo_Giocatore.setBounds(786, 164, 65, 65);
+		icon2 = new ImageIcon("Cerchio_piccolo.png");     //istruzioni necessarie per far comparire l'immagine piccola
+		label_Simbolo_Giocatore.setIcon(icon2);           //istruzioni necessarie per far comparire l'immagine piccola
+		contentPane.add(label_Simbolo_Giocatore);
+		
+		label_Simbolo_Avversario = new JLabel("");
+		label_Simbolo_Avversario.setBounds(786, 302, 65, 65);
+		icon2 = new ImageIcon("X_piccolo.jpg");        //istruzioni necessarie per far comparire l'immagine piccola
+		label_Simbolo_Avversario.setIcon(icon2);       //istruzioni necessarie per far comparire l'immagine piccola
+		contentPane.add(label_Simbolo_Avversario);
+		
 	}
 
-	public JPanel getPanel_Simbolo_Giocatore() {
-		return panel_Simbolo_Giocatore;
+	public JLabel getLabel_Simbolo_Giocatore() {
+		return label_Simbolo_Giocatore;
 	}
 
-	public void setPanel_Simbolo_Giocatore(JPanel panel_Simbolo_Giocatore) {
-		this.panel_Simbolo_Giocatore = panel_Simbolo_Giocatore;
+	public void setLabel_Simbolo_Giocatore(JLabel label_Simbolo_Giocatore) {
+		this.label_Simbolo_Giocatore = label_Simbolo_Giocatore;
+	}
+
+	public JLabel getLabel_Simbolo_Avversario() {
+		return label_Simbolo_Avversario;
+	}
+
+	public void setLabel_Simbolo_Avversario(JLabel label_Simbolo_Avversario) {
+		this.label_Simbolo_Avversario = label_Simbolo_Avversario;
 	}
 
 	public JButton getBtn_Invia() {
@@ -231,14 +196,6 @@ public class Finestra2 extends JFrame {
 
 	public void setBtn_Invia(JButton btn_Invia) {
 		this.btn_Invia = btn_Invia;
-	}
-
-	public JPanel getPanel_Simbolo_Avversario() {
-		return panel_Simbolo_Avversario;
-	}
-
-	public void setPanel_Simbolo_Avversario(JPanel panel_Simbolo_Avversario) {
-		this.panel_Simbolo_Avversario = panel_Simbolo_Avversario;
 	}
 
 	public JLabel getLbl_1() {
