5ce52fdb98f27769227f1dce35a9daf2befba9c1
stefano <stefa@PC-Fisso>
2017-11-07 18:39:39 +0100

    Iniziato gestione classe ServerThread

--- ProgettoTris/src/CONTROLLER/ServerThread.java
+++ ProgettoTris/src/CONTROLLER/ServerThread.java
@@ -8,7 +8,7 @@ import java.net.Socket;
 
 public class ServerThread extends Thread {
 	Socket client = null;
-	String stringaModificata = null;
+	String messaggio2 = null;
 	String messaggio = null;
 	BufferedReader input;
 	PrintWriter output;
@@ -20,20 +20,51 @@ public class ServerThread extends Thread {
 	public void run() {
 		try {
 			input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			
 			while(true) {
 				messaggio = input.readLine(); // riceve il messaggio
-				ServerModel.sendMessage(messaggio, client); // lo ritrasmette 
-				
-				if (messaggio == null || messaggio.equals("FINE")) {
-					System.out.println(Thread.currentThread().getId() + " Addio! " + messaggio);
-					ServerModel.close();
-					System.exit(0);
-				} 
+				//ServerModel.sendMessage(messaggio, client); // lo ritrasmette 
+				input = new BufferedReader(new InputStreamReader(client.getInputStream()));
+				switch(messaggio) {
+					case "1":
+						messaggio2 = input.readLine();
+					break;
+					case "2":
+						messaggio2 = input.readLine();
+						break;
+					case "3":
+						messaggio2 = input.readLine();
+						break;
+					case "4":
+						messaggio2 = input.readLine();
+						break;
+					case "5":
+						messaggio2 = input.readLine();
+						break;
+					case "6":
+						messaggio2 = input.readLine();
+						break;
+					case "7":
+						messaggio2 = input.readLine();
+						break;
+					case "8":
+						messaggio2 = input.readLine();
+						break;
+					case "9":
+						messaggio2 = input.readLine();
+						break;
+					case "FINE":
+						System.out.println(Thread.currentThread().getId() + " Addio! " + messaggio);
+						ServerModel.close();
+						System.exit(0);
+						break;
+					default:
+						System.out.println("Errore scelta!");
+						break;
+				}
 			}		
 			
-		} catch (Exception e) {
-			// esce e basta
+		}catch(Exception e){
+		// esce e basta
 		}
 	}
 }
