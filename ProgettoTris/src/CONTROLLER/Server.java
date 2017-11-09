d8acad4b449eb20769fc05c82a5caa43dc30300e
Stefano <Alunno@PC-12-INF4>
2017-11-09 10:59:57 +0100

    Iniziato gestione server e serverThread

--- ProgettoTris/src/CONTROLLER/Server.java
+++ ProgettoTris/src/CONTROLLER/Server.java
@@ -2,6 +2,7 @@ package CONTROLLER;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -9,22 +10,28 @@ import VIEW.Finestra3;
 
 public class Server implements ActionListener{
 	Finestra3 f3=new Finestra3();
+	ServerSocket serverSocket;
+	Socket socket1,socket2;
+	ServerThread serverThread1,serverThread2;
+	PrintWriter out1;
+	PrintWriter out2;
 	public void start() {
 		
 		f3.getBtnSpegniServer().addActionListener(this);
 		f3.setVisible(true);
 		try {
-			ServerSocket serverSocket = new ServerSocket(6789);
+			serverSocket = new ServerSocket(6789);
 			for (;;) { // Altro modo di fare un ciclo infinito
 				f3.setMod("Server in attesa...");
-				Socket socket = serverSocket.accept();
-				
-				f3.setMod("-> Creo un nuovo thread assegnando il socket: " + socket);
-				ServerThread serverThread = new ServerThread(socket);
-				serverThread.start();
-				
-				// Aggiungo il socket alla lista dei socket
-				ServerModel.listaSocket.add(socket);
+				socket1 = serverSocket.accept();
+				socket2 = serverSocket.accept();			
+				f3.setMod("-> Creo un nuovo thread assegnando il socket: " + socket1);
+				serverThread1 = new ServerThread(socket1);
+				serverThread1.start();
+				serverThread2 = new ServerThread(socket2);
+				serverThread2.start();
+				out1 = new PrintWriter(socket1.getOutputStream(), true);
+				out2 = new PrintWriter(socket2.getOutputStream(), true);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
--- ProgettoTris/src/CONTROLLER/ServerThread.java
+++ ProgettoTris/src/CONTROLLER/ServerThread.java
@@ -1,70 +1,36 @@
 package CONTROLLER;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class ServerThread extends Thread {
 	Socket client = null;
-	String messaggio2 = null;
-	String messaggio = null;
-	BufferedReader input;
+	String messaggio1;
 	PrintWriter output;
+	Scanner in;
 
 	public ServerThread(Socket socket) {
 		this.client = socket;
+		try {
+			output = new PrintWriter(client.getOutputStream(), true);
+			in = new Scanner(client.getInputStream());
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 	public void run() {
-		try {
-			input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-			while(true) {
-				messaggio = input.readLine(); // riceve il messaggio
-				//ServerModel.sendMessage(messaggio, client); // lo ritrasmette 
-				input = new BufferedReader(new InputStreamReader(client.getInputStream()));
-				switch(messaggio) {
-					case "1":
-						messaggio2 = input.readLine();
-					break;
-					case "2":
-						messaggio2 = input.readLine();
-						break;
-					case "3":
-						messaggio2 = input.readLine();
-						break;
-					case "4":
-						messaggio2 = input.readLine();
-						break;
-					case "5":
-						messaggio2 = input.readLine();
-						break;
-					case "6":
-						messaggio2 = input.readLine();
-						break;
-					case "7":
-						messaggio2 = input.readLine();
-						break;
-					case "8":
-						messaggio2 = input.readLine();
-						break;
-					case "9":
-						messaggio2 = input.readLine();
-						break;
-					case "FINE":
-						System.out.println(Thread.currentThread().getId() + " Addio! " + messaggio);
-						ServerModel.close();
-						System.exit(0);
-						break;
-					default:
-						System.out.println("Errore scelta!");
-						break;
-				}
-			}		
-			
-		}catch(Exception e){
-		// esce e basta
+		while (true) {
+			// Leggo una riga e la interpreto
+			String messaggio = in.nextLine();
+			System.out.println("Il server ha ricevuto: " + messaggio + " ");
+
 		}
 	}
 }
